7e126a6bc7f2eb02fda09ae9db0138ab37a61d62
tarun1420 <73821305+tarun1420@users.noreply.github.com>
2020-12-01 10:41:32 +0530

    Add files via upload

---
+++ Day-3(4-11-2020)/InsertInterval.java
@@ -0,0 +1,33 @@
+package com;
+
+import java.util.List;
+
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class InsertInterval {
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+        int i = 0;
+
+        while(i < intervals.size() && intervals.get(i).end < newInterval.start) {
+            i++;
+        }
+
+        while(i < intervals.size() && intervals.get(i).start <= newInterval.end) {
+            newInterval = new Interval(Math.min(intervals.get(i).start, newInterval.start), Math.max(intervals.get(i).end, newInterval.end));
+            intervals.remove(i);
+        }
+
+        intervals.add(i, newInterval);
+
+        return intervals;
+    }
+public static void main(String[] args) {
+}
+}
